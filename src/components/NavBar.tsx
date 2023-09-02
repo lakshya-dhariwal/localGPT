a8f067c83c849ac9f46599441a9e8c04235de54d
lakshya-dhariwal <lakshya@staq.com>
2023-09-02 21:19:48 +0530

    display active chat name

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,14 @@
+import { useAppSelector } from "@src/lib/hooks/redux";
+import { findObjectById } from "@src/lib/util";
+import { useMemo } from "react";
+
 export function NavBar() {
+  const chats = useAppSelector((state) => state.chats.chats);
+  const activeChatId = useAppSelector((state) => state.chats.activeId);
+  console.log(chats);
   return (
     <navbar className="absolute top-0 flex h-[55px] w-full items-center justify-center border-b-2 border-mirage-700">
-      <h1 className="text-center">Hi</h1>
+      <h1 className="text-center ">{chats[activeChatId].summary ?? "Chat"}</h1>
     </navbar>
   );
 }
