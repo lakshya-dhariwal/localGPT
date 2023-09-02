d6d306dd530c7bff9c47a7cd91dfee58bf9ff7ba
lakshya-dhariwal <lakshya@staq.com>
2023-09-03 00:53:13 +0530

    fix crash

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,10 +5,12 @@ import { useMemo } from "react";
 export function NavBar() {
   const chats = useAppSelector((state) => state.chats.chats);
   const activeChatId = useAppSelector((state) => state.chats.activeId);
-
+  //chats?.activeChatId.summary ??
   return (
     <nav className="absolute top-0 flex h-[55px] w-full items-center justify-center border-b-2 border-mirage-700">
-      <h1 className="text-center ">{chats?.activeChatId.summary ?? "Chat"}</h1>
+      <h1 className="text-center ">
+        {chats.activeChatId ? chats?.activeChatId.summary : "New Chat"}
+      </h1>
     </nav>
   );
 }
